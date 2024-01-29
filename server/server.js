cd540824e04902e8f07f76b40f50f5cc80b424a0
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-29 13:15:32 +0200

    building the ui for the cards

--- server/server.js
+++ server/server.js
@@ -1,40 +1,42 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import Resource from "./Schema/Resource.js";
-import cors from 'cors'
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const server = express()
+("J581ec7DDz9d2OkrtdI8e2yQKUebkQkjjIycnwFfK337Z0nfSGfIG9fP");
+const server = express();
 let PORT = 5000;
 
 // allow for cross origing resource sharing
 server.use(cors());
-server.use(express.json())
+server.use(express.json());
 
-mongoose.connect(process.env.DB_LOCATION, {
-    autoIndex: true
-})
-.then(() => console.log('MongoDB connection successful'))
-.catch((err) => console.error('MongoDB connection error: ', err));
-
+mongoose
+  .connect(process.env.DB_LOCATION, {
+    autoIndex: true,
+  })
+  .then(() => console.log("MongoDB connection successful"))
+  .catch((err) => console.error("MongoDB connection error: ", err));
 
 // get request to withdraw the data from mongodb
-server.get('/get_resources', (req, res) => {
-    const limitCount = 5;
+server.get("/get_resources", (req, res) => {
+  const limitCount = 10;
 
-    Resource.find({}).limit(limitCount)
-    .then(resources => {
-        res.status(200).json((resources))
+  Resource.find({})
+    .limit(limitCount)
+    .then((resources) => {
+      res.status(200).json(resources);
     })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({ error: err.message })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err.message });
+    });
+});
 
 // post listening and giving a callback message
 server.listen(PORT, () => {
-    console.log(`listening to port => ${PORT}`);
-})
+  console.log(`listening to port => ${PORT}`);
+});
